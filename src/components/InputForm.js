b7b134d383c58ca37315173127a64ff80f4629da
thuongphv2312 <44487146+thuongphv2312@users.noreply.github.com>
2020-10-15 19:24:21 +0700

    update

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -7,7 +7,8 @@ const InputForm = (props) => {
     <Container className='inputForm'>
       <Form inline>
         <FormControl type="text" placeholder="Type your task" className=" mr-sm-2" onChange={(e) => setTask(e.target.value)} value={task}/>
-        <Button onClick={()=>props.onSubmit(task)}>Add</Button>
+        <Button onClick={() =>
+        { props.onSubmit(task);setTask('')}}>Add</Button>
       </Form>
     </Container>
   );
